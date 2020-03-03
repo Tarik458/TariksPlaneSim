4633e849e3049c3840cb545844c42cbe9e9c1a98
Tarik Sabin <tarik.sabin@gmail.com>
2020-03-03 07:46:44 +0000

    Got the front landing gear animation working.

---
+++ Assets/GearHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearHandler : MonoBehaviour
+{
+    public Animator animator;
+    public bool gearUp = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            if (gearUp == false)
+            {
+                animator.SetBool("isAnimating", true);
+                gearUp = true;
+            }
+            else if (gearUp == true)
+            {
+                animator.SetBool("isAnimating", false);
+                gearUp = false;
+            }
+        }
+    }
+}
---
+++ Assets/PlaneHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneHandler : MonoBehaviour
+{
+    public GameObject Plane;
+    public float Speed = 0;
+
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && Speed < 200)
+        {
+            Speed++;
+        }
+        if (Input.GetKey(KeyCode.LeftControl) && Speed > 0)
+        {
+            Speed--;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            Plane.transform.Rotate(-1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Plane.transform.Rotate(1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Plane.transform.Rotate(0f, 0.5f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Plane.transform.Rotate(0f, -0.5f, 0f);
+        }
+        if (Input.GetKey(KeyCode.Q) && Speed > 0)
+        {
+            Plane.transform.Rotate(0f, 0f, 0.3f);
+        }
+        if (Input.GetKey(KeyCode.E) && Speed > 0)
+        {
+            Plane.transform.Rotate(0f, 0f, -0.3f);
+        }
+
+        Plane.transform.Translate(Vector3.up * Time.deltaTime * Speed);
+    }
+}
